c9d180f7f787c7802656cd95883cd6e93af760d9
MakhmoodSodikov <sodikov.mmo@phystech.edu>
2021-03-22 15:38:05 +0300

    fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, abort, redirect, url_for, flash
 from model import load, infer
-
+app = Flask(__name__)
 
 @app.route('/')
 def check_is_running():
@@ -24,6 +24,5 @@ def hello_world():
     except:
         return redirect(url_for('bad_request'))
 
-if __name__ == "__main__":
-    app = Flask(__name__)
-    app.run()
+
+app.run()
